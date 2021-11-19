116c2062f8b72365232e5842ec51aebbc44689a9
Babak <babak@Babaks-MacBook-Pro.local>
2021-11-18 17:54:58 -0800

    Add DB setup

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    message = models.CharField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
